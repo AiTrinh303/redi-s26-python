821c63f72bdc7a783f083fb0183162cc04d89156
Ai Trinh <trinhnguyentrongai@gmail.com>
2026-03-19 21:01:48 +0100

    problem11: Update solution

--- Common_Elements.py
+++ Common_Elements.py
@@ -1,11 +1,24 @@
+# Problem 11: Common Elements
+# Find common elements in two lists.
 
+# Input:
+# a = [1, 2, 3, 4]
+# b = [3, 4, 5, 6]
+# Output: [3, 4]
 
 a = [1, 2, 3, 4]
 b = [3, 4, 5, 6]
-common_elements = []
+# common_elements = []
 
-for elem in a:
-    if elem in b:
-        common_elements.append(elem)
-        
-print(common_elements)
+# for elem in a:
+#     if elem in b:
+#         common_elements.append(elem)
+
+# print(common_elements)
+
+a = [1, 2, 3, 4]
+b = [3, 4, 5, 6]
+
+common = list(set(a) & set(b))
+
+print(common)
