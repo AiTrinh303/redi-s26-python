91b0272ba209037d6846fce0bf85507c413fa62e
Ai Trinh <trinhnguyentrongai@gmail.com>
2026-03-24 13:19:07 +0100

    problem2:sum of elements

--- Sum_of_Elements.py
+++ Sum_of_Elements.py
@@ -1,3 +1,9 @@
 # Problem 2: Sum of Elements
 # Given a list of integers, compute the sum of all elements without using sum().
 
+numbers = [1, 2, 3, 4, 5]
+total = 0
+for num in numbers:
+    total += num
+print(total)
+
